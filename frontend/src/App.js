a1440084ac9ecb3174db2d7860bb1dcec33a3b97
Lissette <lissete.deleon@gmail.com>
2023-04-06 21:55:40 -0500

    testing-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">PAW-WORLD</a>
+      </header>
+      <main>list of products</main>
+    </div>
+  );
+}
+
+export default App;
